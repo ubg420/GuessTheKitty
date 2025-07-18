9657820a34b3bee7e52ec529442cc3f63ecdd3e2
ubg420 <antibanh4x@gmail.com>
2025-07-18 21:07:48 +0900

    1

---
+++ version.js
@@ -0,0 +1,2 @@
+version="2";
+libs=["phaser-ads.min.js","https://cdn.jsdelivr.net/npm/@orange-games/phaser-cachebuster@2.0/build/phaser-cachebuster.min.js","https://cdn.jsdelivr.net/npm/@orange-games/phaser-nineslice@2.0/build/phaser-nineslice.min.js","https://cdn.jsdelivr.net/npm/@orange-games/phaser-spine@3.0/build/phaser-spine.min.js","https://cdn.jsdelivr.net/npm/@orange-games/phaser-super-storage@1.0/build/phaser-super-storage.min.js","https://cdn.jsdelivr.net/npm/@orange-games/splash@3.5/build/splash.min.js"];
